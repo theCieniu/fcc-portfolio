9eb89e37e4786357c86412fa21ad79bfdea8e44c
theCieniu <pweiher@wp.eu>
2019-08-29 21:19:01 +0200

    typing effect

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+
+var i = 0;
+text = "My journey to become a web developer";
+
+function typing() {
+    if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 100);
+  }
+}
+
+typing()
+
+
 // document.addEventListener("scroll", alert("first test"))
